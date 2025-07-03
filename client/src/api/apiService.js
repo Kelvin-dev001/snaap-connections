1a3f27e20676e7d95858492dd165fe4635a5f35d
Kelvin-dev001 <kelvinoyugi101@gmail.com>
2025-07-03 04:37:23 +0300

    apiservice

--- client/src/api/apiService.js
+++ client/src/api/apiService.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 const apiBaseUrl =
   process.env.REACT_APP_API_URL ||
   (process.env.NODE_ENV === 'development'
-    ? 'http://localhost:5000/api'
+    ? 'https://localhost:5000/api'
     : undefined);
 
 if (!apiBaseUrl) {
