63de69aa2e124af443843677ead902cb88ea45e5
Kelvin-dev001 <kelvinoyugi101@gmail.com>
2025-07-18 17:41:56 +0300

    logintry

--- snaap-backend/server.js
+++ snaap-backend/server.js
@@ -21,12 +21,7 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: function(origin, callback) {
-    // Allow requests with no origin (like mobile apps, curl, etc.)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.includes(origin)) return callback(null, true);
-    return callback(new Error('Not allowed by CORS'));
-  },
+  origin: allowedOrigins,
   credentials: true
 }));
 
